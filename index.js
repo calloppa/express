cee21468c82a9a43412f3311e3508beec781b97b
calloppa <49674065+calloppa@users.noreply.github.com>
2019-05-29 20:40:01 +0800

    change

--- index.js
+++ index.js
@@ -13,5 +13,76 @@ app.get("/hi",(request,response) => {
 	response.send("Hello world!");
 });
 
+const CONNECTION URL = "mongodb+srv://calloppa:<password>@tiffanylab10-awd1d.mongodb.net/test?retryWrites=true";
+const DATABASE_ NAME = "TiffanyLab10" ;
+//save a new note
+app.post("/notes", (request, response) => {
+
+	MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true ], (error, client) => {
+		if(error) {
+					response.send(error);
+					throw error;
+		}
+		database = client.db(DATABASE_NAME);
+		collection = database.collection("Notes");
+
+		collection.insert(request.body,(error,result) => {
+			if(error) {
+						return response.status(500).send(error);
+			}
+			response.send(result.result);
+		}); 
+	});
+});  
+//we will use the following template for notes: '{"name":"", " body":""}'
+
 //this last line is required by zeit since we are stateless
 module.exports = app;
+
+//get all notes
+app.get("/notes", (request, response) => {
+
+	MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+	if(error) {
+				response.send(error);
+				throw error;
+	}
+	database = client.db(DATABASE_NAME);
+	collection = database.collection("Notes");
+
+	collection.find({}).toArray((error,result) => {
+	if(error){
+					return response.tatus(500).send(error);
+	}
+	response.}send(result);
+		});
+	}); 
+});
+
+//get a single note
+app.get("/notes/:id",(request,response) => {
+	
+	MongoClient.connect(CONNECTION_URL, { useNewUrlParser:true},(error,client) => {
+	if(error) {
+		response.send(error);
+		throw error;
+	}
+	database = client.db(DATABASE_NAME);
+	collection = database.collection("Notes");
+
+	collection.find({}).toArray((error, result) => {
+	if(error){
+		return response.status(500).send(error);
+	}
+
+	//parse the request into a number
+	var numberID = parseInt (request.params.id); 
+
+	//only return a response if it is valid
+	if (numberID >= result.length)
+		response.send("Not enough elements in database")
+	else
+		response.send(result[numberID]);
+		});
+	});
+});
