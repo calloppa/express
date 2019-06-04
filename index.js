a528b1818b448c5c6b2672bdfbfa3e60924df441
calloppa <49674065+calloppa@users.noreply.github.com>
2019-06-04 15:08:34 +0800

    1

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get("/notes", (request, response) => {
 
 	collection.find({}).toArray((error,result) => {
 	if(error){
-					return response.tatus(500).send(error);
+					return response.status(500).send(error);
 	}
 	response.send(result);
 		});
